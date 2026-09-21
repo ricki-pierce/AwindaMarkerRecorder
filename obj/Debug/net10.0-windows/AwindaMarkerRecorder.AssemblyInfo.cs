a3063c13a08522eaea617e15341e165b8bf2f822
ricki-pierce <rpier12@lsu.edu>
2026-09-21 11:15:55 -0500

    Initial project upload

--- obj/Debug/net10.0-windows/AwindaMarkerRecorder.AssemblyInfo.cs
+++ obj/Debug/net10.0-windows/AwindaMarkerRecorder.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AwindaMarkerRecorder")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e26bceff34a299b4bc5d1f89ea18102047df7cb6")]
 [assembly: System.Reflection.AssemblyProductAttribute("AwindaMarkerRecorder")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AwindaMarkerRecorder")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
